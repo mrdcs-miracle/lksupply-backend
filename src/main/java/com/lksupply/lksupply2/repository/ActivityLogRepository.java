310adcb090a654433089e72a3f80aac41e53f92e
Thisandu Hettiarachchi <htalokya@students.nsbm.ac.lk>
2026-02-11 13:14:37 +0530

    Add entity of this supply chain lk

--- src/main/java/com/lksupply/lksupply2/repository/ActivityLogRepository.java
+++ src/main/java/com/lksupply/lksupply2/repository/ActivityLogRepository.java
@@ -1,6 +1,5 @@
 package com.lksupply.lksupply2.repository;
 
-import com.lksupply.lksupply2.entity.ActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
